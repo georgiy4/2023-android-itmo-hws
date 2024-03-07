20e1d5cc5add47571b47c872581c048b1d7b1b83
georgiy4 <115575494+georgiy4@users.noreply.github.com>
2024-03-08 01:01:09 +0300

    Add files via upload

--- app/src/main/java/ru/ok/itmo/example/MainActivity.kt
+++ app/src/main/java/ru/ok/itmo/example/MainActivity.kt
@@ -1,6 +1,62 @@
 package ru.ok.itmo.example
 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity(R.layout.activity) {
+
+    companion object {
+        const val correctLogin = "geor"
+        const val correctPassword = "123456"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val button = findViewById<Button>(R.id.button)
+        val viewLogin = findViewById<EditText>(R.id.text_login)
+        val viewPassword = findViewById<EditText>(R.id.text_password)
+
+        viewPassword.setOnEditorActionListener { _, _, _ ->
+            button.callOnClick()
+            false
+        }
+
+        button?.setOnClickListener {
+            val login = viewLogin.text.toString()
+            val password = viewPassword.text.toString()
+            if (login == "") {
+                showMessage(getString(R.string.no_login))
+            } else if (password == ""){
+                showMessage(getString(R.string.no_password))
+            } else if (password.length < 6){
+                showMessage(getString(R.string.password_must_contain_6_digits))
+            } else if (login != correctLogin) {
+                showMessage(getString(R.string.incorrect_login))
+            } else if (password != correctPassword) {
+                showMessage(getString(R.string.incorrect_password))
+            } else {
+                TODO()
+            }
+        }
+
+        findViewById<RadioButton>(R.id.rb_theme_dark).setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        findViewById<RadioButton>(R.id.rb_theme_light).setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+        findViewById<RadioButton>(R.id.rb_theme_system).setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
+    }
+
+    private fun showMessage(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 }
