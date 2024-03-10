ed2c2c71d0a5d78608f21382fbf8e90fa388cc80
georgiy4 <115575494+georgiy4@users.noreply.github.com>
2024-03-10 19:27:32 +0300

    Delete app/src/main/java/ru/ok/itmo/example/DataChat.kt

--- app/src/main/java/ru/ok/itmo/example/DataChat.kt
+++
@@ -1,8 +0,0 @@
-package ru.ok.itmo.example
-
-import com.google.gson.annotations.SerializedName;
-import kotlinx.serialization.Serializable
-
-@Serializable
-class DataChat(@SerializedName("name") var name: String = "", @SerializedName("image") var image: String = "") {
-}
